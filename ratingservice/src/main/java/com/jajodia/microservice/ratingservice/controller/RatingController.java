fc357420601fc763fe29b0e18f68b9d09cabe4f8
ayush.jajdodia <ayush.jajodia@thepsi.com>
2023-03-29 22:34:05 +0530

    circuit breaker implementation

--- ratingservice/src/main/java/com/jajodia/microservice/ratingservice/controller/RatingController.java
+++ ratingservice/src/main/java/com/jajodia/microservice/ratingservice/controller/RatingController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/ratingService")
